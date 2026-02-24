81ea9b62ee39515c5d8a9ce6dec4f94b34460265
GAURAVKADAM007 <gkadam3847@gmail.com>
2026-02-24 22:09:40 +0530

    Updated the utility files for creating docker container

--- src/main/java/com/myproject/crudapp/utils/JDBCUtils.java
+++ src/main/java/com/myproject/crudapp/utils/JDBCUtils.java
@@ -5,9 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCUtils {
-    private static final String URL = "jdbc:postgresql://localhost:5432/bookdb";
-    private static final String USER = "postgres";
-    private static final String PASS = "1234";
+    private static final String JDBC_HOST = System.getenv("JDBC_HOST") != null ? System.getenv("JDBC_HOST") : "localhost";
+    private static final String JDBC_PORT = System.getenv("JDBC_PORT") != null ? System.getenv("JDBC_PORT") : "5432";
+    private static final String JDBC_DB = System.getenv("JDBC_DB") != null ? System.getenv("JDBC_DB") : "bookdb";
+    private static final String USERNAME = System.getenv("JDBC_USERNAME") != null ? System.getenv("JDBC_USERNAME") : "postgres";
+    private static final String PASSWORD = System.getenv("JDBC_PASSWORD") != null ? System.getenv("JDBC_PASSWORD") : "1234";
+
+    private static final String URL = "jdbc:postgresql://" + JDBC_HOST + ":" + JDBC_PORT + "/" + JDBC_DB;
 
     static {
         try {
@@ -18,7 +22,7 @@ public class JDBCUtils {
     }
 
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(URL,USER,PASS);
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
     }
 
 }
